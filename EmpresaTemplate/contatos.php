a41b9592cf1da239c231857006191d639c344722
hugoresende27 <Hugoresende27@gmail.com>
2021-11-30 19:17:19 +0000

    a

--- EmpresaTemplate/contatos.php
+++ EmpresaTemplate/contatos.php
@@ -1,3 +1,32 @@
+<?php
+    if (empty($_POST) == false) {//se o post contem dados , empty == falso
+        //echo 'tem dados!';
+        print_r($_POST);
+    }
+?>
 
+<h1>CONTATOS</h1>
+<!--
+<form action="" method="get">  form é um conjunto de inputs que vai ser tratado na action, e o method pode ser get ou posto
+                        action para onde vão ser enviados os dados do formulário
 
-    <h1>CONTATOS</h1>
+    <input type="text" name="nome">
+    <input type="text" name="apelido">
+    <input type="email" name="email"> email em HTML5 pede uma formatação com %@%
+    <input type="password" name="pass">
+    <input type="number" name="num">
+    <input type="range" name="range">
+    <input type="search" name="search">
+    <input type="date" name="datas">
+
+</form>
+//////////////////////////////////////////////////////////////////////
+METODO GET todos os valores de input vão aparecer na query string
+METODO POST usado mais frequentemente
+-->
+
+<form action="" method="post">
+    <input type="text" name="userName">
+    <input type="password" name="userPass">
+    <input type="submit" value="Entrar">
+</form>
